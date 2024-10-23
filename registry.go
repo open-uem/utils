1497c2a22b654b089325144a0d406b28bc9134b0
Miguel Cabrerizo <doncicuto@gmail.com>
2024-10-23 08:59:18 +0200

    feat: add registry utils

---
+++ registry.go
@@ -0,0 +1,16 @@
+package openuem_utils
+
+import "golang.org/x/sys/windows/registry"
+
+func OpenRegistryForQuery(key registry.Key, path string) (registry.Key, error) {
+	return registry.OpenKey(key, path, registry.QUERY_VALUE)
+}
+
+func GetValueFromRegistry(k registry.Key, key string) (string, error) {
+	s, _, err := k.GetStringValue(key)
+	if err != nil {
+		return "", err
+	}
+
+	return s, nil
+}
