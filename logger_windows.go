7db17d9d36d05ea7250efcf19ec927b4fe5f442d
Miguel Cabrerizo <doncicuto@gmail.com>
2025-11-25 12:47:44 +0100

    feat: add auth windows logger

--- logger_windows.go
+++ logger_windows.go
@@ -3,6 +3,7 @@
 package utils
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"path/filepath"
@@ -39,4 +40,25 @@ func NewLogger(logFilename string) *OpenUEMLogger {
 
 func (l *OpenUEMLogger) Close() {
 	l.LogFile.Close()
+}
+
+func NewAuthLogger() *log.Logger {
+	// Get executable path to store logs
+	ex, err := os.Executable()
+	if err != nil {
+		log.Fatalf("could not get executable info: %v", err)
+	}
+	wd := filepath.Dir(ex)
+
+	if _, err := os.Stat(wd); os.IsNotExist(err) {
+		if err := os.MkdirAll(wd, 0600); err != nil {
+			log.Fatalf("[FATAL]: could not create log directory, reason: %v", err)
+		}
+	}
+
+	f, err := os.OpenFile(filepath.Join(wd, "openuem-auth"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0660)
+	if err != nil {
+		fmt.Println("auth log file not created", err.Error())
+	}
+	return log.New(f, "", log.Ldate|log.Ltime)
 }
