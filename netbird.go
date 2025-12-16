6ba25c99e4efa8cfb04c155b7428f1a3cf7d5768
Miguel Cabrerizo <doncicuto@gmail.com>
2025-12-16 16:57:30 +0100

    fix: remove debug for netbird peer exists

--- netbird.go
+++ netbird.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
 	"strings"
 )
@@ -201,8 +200,6 @@ func NetBirdPeerExists(name string, managementURL string, token string) (bool, e
 	if err != nil {
 		return false, err
 	}
-
-	log.Println("Body: ", string(body))
 
 	peers := []NetBirdPeer{}
 	if err := json.Unmarshal(body, &peers); err != nil {
